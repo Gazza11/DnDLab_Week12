de3509022d6b9a1263cf907efb4e891aaa675f72
Gary Darnell <gdarnell9@gmail.com>
2021-07-28 10:19:04 +0100

    Initial commit with baseCharacter.

---
+++ src/main/java/BaseCharacter.java
@@ -0,0 +1,30 @@
+public class BaseCharacter {
+
+    private String name;
+    private int HP;
+    private int baseAttack;
+    private int baseResistance;
+
+    public BaseCharacter(String name, int HP, int baseAttack, int baseResistance) {
+        this.name = name;
+        this.HP = HP;
+        this.baseAttack = baseAttack;
+        this.baseResistance = baseResistance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public int getBaseAttack() {
+        return baseAttack;
+    }
+
+    public int getBaseResistance() {
+        return baseResistance;
+    }
+}
---
+++ src/test/java/BaseCharacterTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BaseCharacterTest {
+
+    BaseCharacter baseCharacter;
+
+    @Before
+    public void before(){
+        baseCharacter = new BaseCharacter("Hero", 10, 5, 2);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Hero", baseCharacter.getName());
+    }
+
+    @Test
+    public void hasHP(){
+        assertEquals(10, baseCharacter.getHP());
+    }
+
+    @Test
+    public void hasBaseAttack(){
+        assertEquals(5, baseCharacter.getBaseAttack());
+    }
+
+    @Test
+    public void hasBaseResistance(){
+        assertEquals(2, baseCharacter.getBaseResistance());
+    }
+}
